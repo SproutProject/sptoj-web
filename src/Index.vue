f5bac23678b68dfa47fb65867ff2a233ca12f442
PZ Read <netfirewall@gmail.com>
2017-02-21 16:57:36 +0800

    Fix blank page.

--- src/Index.vue
+++ src/Index.vue
@@ -1,22 +1,7 @@
 <template>
-  <div id="index">
-    <img src="./assets/logo.png">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://gitter.im/vuejs/vue" target="_blank">Gitter Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
-  </div>
+<div id="index" class="grid">
+<div class="col">This page is intentionally left blank.</div>
+</div>
 </template>
 
 <script lang="ts">
@@ -24,29 +9,5 @@ import * as Vue from 'vue';
 import { Component } from 'vue-property-decorator'
 
 @Component
-export default class Index extends Vue {
-  data(): Object {
-    return {
-      msg: 'Welcome to Your Vue.js App'
-    }
-  }
-}
+export default class Index extends Vue {}
 </script>
-
-<style lang="less">
-@font-color: #2c3e50;
-@link-color: #42b983;
-
-#index {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: @font-color;
-  margin-top: 60px;
-}
-
-h1, h2 {
-  font-weight: normal;
-}
-</style>
